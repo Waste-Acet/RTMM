ea0aed847e2ae0e42f4df3efc4b67759d42de51c
Boopathi <112680334+Waste-Acet@users.noreply.github.com>
2024-06-17 15:49:07 +0530

    Update index1.py

--- index1.py
+++ index1.py
@@ -4,8 +4,8 @@ import time
 import pandas as pd
 import numpy as np
 from PIL import Image
-import plotly.graph_objects as go
-import plotly.express as px
+#import plotly.graph_objects as go
+#import plotly.express as px
 import random
 import base64
 
